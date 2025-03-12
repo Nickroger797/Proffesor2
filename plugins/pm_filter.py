f6aacf7c39c4ee1b99272484dc4fa7fdbf9d17de
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-12 12:01:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -19,6 +19,7 @@ from database.gfilters_mdb import find_gfilter, get_gfilters, del_allg
 from urllib.parse import quote_plus
 from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
 import urllib.parse
+from info import BLOGSPOT_URL  # Make sure only this is imported
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
