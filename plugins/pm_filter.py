7e942ae0095e2fac1934f983674e129be3ca8fa5
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-14 10:41:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1382,53 +1382,42 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     encoded_url = urllib.parse.quote_plus(redirect_url)
                     blog_url = f"{BLOGSPOT_URL}?url={encoded_url}"
                     await query.answer(url=blog_url)
+                    return
+                else:
+                    await query.answer(f"Hᴇʏ {query.from_user.first_name}, Tʜɪs Is Nᴏᴛ Yᴏᴜʀ Mᴏᴠɪᴇ Rᴇǫᴜᴇsᴛ. Rᴇǫᴜᴇsᴛ Yᴏᴜʀ's !", show_alert=True)
+
+            elif settings['is_shortlink'] and await db.has_premium_access(query.from_user.id):
+                if clicked == typed:
+                    redirect_url = f"https://telegram.me/{temp.U_NAME}?start={ident}_{file_id}"
+                    encoded_url = urllib.parse.quote_plus(redirect_url)
+                    blog_url = f"{BLOGSPOT_URL}?url={encoded_url}"
                     await query.answer(url=blog_url)
                     return
                 else:
                     await query.answer(f"Hᴇʏ {query.from_user.first_name}, Tʜɪs Is Nᴏᴛ Yᴏᴜʀ Mᴏᴠɪᴇ Rᴇǫᴜᴇsᴛ. Rᴇǫᴜᴇsᴛ Yᴏᴜʀ's !", show_alert=True)
-            elif settings['is_shortlink'] and await db.has_premium_access(query.from_user.id):
-                if clicked == typed:
-                    try:
-                        redirect_url = f"https://telegram.me/{temp.U_NAME}?start={ident}_{file_id}"
-                        encoded_url = urllib.parse.quote_plus(redirect_url)
-                        blog_url = f"{BLOGSPOT_URL}?url={encoded_url}"
-                        await query.answer(url=blog_url)
-                    except:
-                        await query.answer("Something went wrong!", show_alert=True)
-                    return
-                else:
-                    await query.answer(f"Hᴇʏ {query.from_user.first_name}, Tʜɪs Is Nᴏᴛ Yᴏᴜʀ Mᴏᴠɪᴇ Rᴇǫᴜᴇsᴛ. Rᴇǫᴜᴇsᴛ Yᴏᴜʀ's !", show_alert=True)
-                    
+
             else:
                 if clicked == typed:
-                    try:
-                        redirect_url = f"https://telegram.me/{temp.U_NAME}?start={ident}_{file_id}"
-                        encoded_url = urllib.parse.quote_plus(redirect_url)
-                        blog_url = f"{BLOGSPOT_URL}?url={encoded_url}"
-                        await query.answer(url=blog_url)
-                    except:
-                        await query.answer("Something went wrong!", show_alert=True)
+                    redirect_url = f"https://telegram.me/{temp.U_NAME}?start={ident}_{file_id}"
+                    encoded_url = urllib.parse.quote_plus(redirect_url)
+                    blog_url = f"{BLOGSPOT_URL}?url={encoded_url}"
+                    await query.answer(url=blog_url)
                     return
                 else:
                     await query.answer(f"Hᴇʏ {query.from_user.first_name}, Tʜɪs Is Nᴏᴛ Yᴏᴜʀ Mᴏᴠɪᴇ Rᴇǫᴜᴇsᴛ. Rᴇǫᴜᴇsᴛ Yᴏᴜʀ's !", show_alert=True)
-        except UserIsBlocked:
-            await query.answer('Uɴʙʟᴏᴄᴋ ᴛʜᴇ ʙᴏᴛ ᴍᴀʜɴ !', show_alert=True)
-        except PeerIdInvalid:
-            try:
+
+            except UserIsBlocked:
+                await query.answer('Uɴʙʟᴏᴄᴋ ᴛʜᴇ ʙᴏᴛ ᴍᴀʜɴ !', show_alert=True)
+            except PeerIdInvalid:
                 redirect_url = f"https://telegram.me/{temp.U_NAME}?start={ident}_{file_id}"
                 encoded_url = urllib.parse.quote_plus(redirect_url)
                 blog_url = f"{BLOGSPOT_URL}?url={encoded_url}"
                 await query.answer(url=blog_url)
-            except:
-                await query.answer("Something went wrong!", show_alert=True)
-        except Exception as e:
-            try:
+            except Exception as e:
                 redirect_url = f"https://telegram.me/{temp.U_NAME}?start={ident}_{file_id}"
                 encoded_url = urllib.parse.quote_plus(redirect_url)
                 blog_url = f"{BLOGSPOT_URL}?url={encoded_url}"
                 await query.answer(url=blog_url)
-            except:
-                await query.answer("Something went wrong!", show_alert=True)
     
     elif query.data.startswith("sendfiles"):
         clicked = query.from_user.id
